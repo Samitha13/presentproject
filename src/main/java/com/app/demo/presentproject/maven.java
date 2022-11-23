68fd7db41a9f684e6c1f24eb2bfc3a6c2efe40b8
samitha13 <samithagade@gmail.com>
2022-11-23 13:48:55 +0530

    demo for team

---
+++ src/main/java/com/app/demo/presentproject/maven.java
@@ -0,0 +1,11 @@
+package com.app.demo.presentproject;
+
+public class maven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World! ");
+
+	}
+
+}
